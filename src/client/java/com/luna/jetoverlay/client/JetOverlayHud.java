f26553abd7b37fd1a4e220bb098cc446ec45c3f7
Klemmbaustein <jan.paschke@outlook.de>
2024-08-14 01:15:18 +0200

    Added a function that renders a UI text at a 3d point in space

--- src/client/java/com/luna/jetoverlay/client/JetOverlayHud.java
+++ src/client/java/com/luna/jetoverlay/client/JetOverlayHud.java
@@ -1,66 +1,76 @@
 package com.luna.jetoverlay.client;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.*;
-import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
 import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.entity.DisplayRenderer;
-import net.minecraft.client.renderer.entity.EntityRenderer;
-import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.memory.NearestVisibleLivingEntities;
 import net.minecraft.world.entity.ai.targeting.TargetingConditions;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
-import org.apache.logging.log4j.core.pattern.TextRenderer;
 import org.joml.*;
-import java.awt.*;
-import java.lang.Math;
+
 import java.util.List;
 
 public class JetOverlayHud implements HudRenderCallback {
-    boolean shouldDraw = false;
+	boolean shouldDraw = false;
 
-    @Override
+	protected void drawTextAt(String text, Vector3f worldPosition, GuiGraphics drawContext) {
+		Minecraft mc = Minecraft.getInstance();
+		// Matrix math shamelessly stolen from here:
+		// https://github.com/Klemmbaustein/Klemmgine/blob/f5454be1e95c43cbb91f0f55abf682774572defe/EngineSource/Objects/Components/CameraComponent.cpp#L36-L43
+		// And here:
+		// https://github.com/Klemmbaustein/Klemmgine/blob/f5454be1e95c43cbb91f0f55abf682774572defe/EngineSource/Rendering/Camera/Camera.cpp#L41-L43
+		Camera cam = mc.gameRenderer.getMainCamera();
+		Matrix4f projection = mc.gameRenderer.getProjectionMatrix(mc.options.fov().get());
+		Matrix4f view = new Matrix4f().identity();
+		view.setLookAt(cam.getLookVector(), new Vector3f(0, 0, 0), new Vector3f(0, 1, 0));
+		view.translate(cam.getPosition().toVector3f());
+		Matrix4f viewProjection = projection.mul(view);
+
+		Vector4f pos = viewProjection.transform(new Vector4f(
+				-worldPosition.x,
+				-worldPosition.y,
+				-worldPosition.z,
+				1));
+
+		// Return if the depth (z coordinate) is less than 0
+		// So if the position is behind the camera.
+		if (pos.z < 0)
+			return;
+
+		float x = pos.x / pos.z, y = pos.y / pos.z;
+
+		int windowWidth = drawContext.guiWidth(), windowHeight = drawContext.guiHeight();
+
+		// Convert to UI coordinates. So from -1 - 1 to 0 - width/height
+		int xPos = (int) (x * (windowWidth / 2.0f)) + windowWidth / 2;
+		int yPos = (int) (y * (windowHeight / 2.0f)) + windowHeight / 2;
+
+		xPos -= mc.font.width(text) / 2;
+		drawContext.drawString(mc.font, text, xPos, yPos, 0xffffffff);
+	}
+
+	@Override
     public void onHudRender(GuiGraphics drawContext, float tickDelta) {
-        int y;
-        int x;
-        Minecraft client = Minecraft.getInstance();
+		var worldIn = Minecraft.getInstance().level;
+		Player player = Minecraft.getInstance().player;
 
-        if (client != null) {
-            int width = client.getWindow().getGuiScaledWidth();
-            int height = client.getWindow().getGuiScaledHeight();
-            x = width / 2;
-            y = height;
-            int range = 20;
-            BlockPos pos = Minecraft.getInstance().player.blockPosition();
-            Camera camera = Minecraft.getInstance().gameRenderer.getMainCamera();
-            LocalPlayer player = Minecraft.getInstance().player;
+		if (worldIn == null || player == null) {
+			return;
+		}
 
-            var worldIn = Minecraft.getInstance().level;
-            List<LivingEntity> entities = worldIn.getNearbyEntities(LivingEntity.class,
-                    TargetingConditions.DEFAULT,
-                    player,
-                    new AABB(pos.subtract(new BlockPos(range, range, range)), pos.offset(new BlockPos(range, range, range)))
-            );
-            Camera renderInfo = Minecraft.getInstance().gameRenderer.getMainCamera();
-        }
+		int range = 25;
+		BlockPos pos = player.blockPosition();
+		List<LivingEntity> entities = worldIn.getNearbyEntities(LivingEntity.class,
+				TargetingConditions.DEFAULT,
+				player,
+				new AABB(pos.subtract(new BlockPos(range, range, range)), pos.offset(new BlockPos(range, range, range)))
+		);
 
+		for (var entity : entities) {
+			drawTextAt("Health: " + entity.getHealth(), entity.position().toVector3f().add(0, 2.5f, 0), drawContext);
+		}
     }
-
-
-
 }
