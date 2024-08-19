c736e6d35239c171c36494c05986443c92028ae3
Luna <lumi.kattlin@gmail.com>
2024-08-19 11:21:41 +0100

    Changed funny name

--- src/main/java/com/luna/jetoverlay/blocks/RotationToRedstone.java
+++ src/main/java/com/luna/jetoverlay/blocks/RotationToRedstone.java
@@ -2,10 +2,10 @@ package com.luna.jetoverlay.blocks;
 
 import net.minecraft.world.level.block.Block;
 
-public class JavaOutputter extends Block {
+public class RotationToRedstone extends Block {
 
 
-    public JavaOutputter(Properties properties) {
+    public RotationToRedstone(Properties properties) {
         super(properties);
     }
 
